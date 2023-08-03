8de12ad6dace698006e709a931e9f82d794eb24a
EChilin5 <edgar3ac@gmail.com>
2023-08-02 22:30:39 -0700

    home page intial design

--- frontend/src/components/homePageComponents/ResourcesSection.js
+++ frontend/src/components/homePageComponents/ResourcesSection.js
@@ -6,15 +6,21 @@ import natThree from "../../sass/img/nat-3-large.jpg";
 const ResourcesSection = () => {
   return (
     <div className="resource-section">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">Our Services</h2>
+      </div>{" "}
       <div className="row">
         <div className="col-1-of-3">
-          <img src={natOne} alt="natOne" />
+          <img src={natOne} alt="natOne" className="resouce-group" />
+          <h3>Recipes</h3>
         </div>
         <div className="col-1-of-3">
-          <img src={natOne} alt="natOne" />
+          <img src={natTwo} alt="natOne" className="resouce-group" />
+          <h3>Tracker</h3>
         </div>
-        <div className="col-1-of-3">
-          <img src={natOne} alt="natOne" />
+        <div className="col-1-of-3 resouce-group">
+          <img src={natThree} alt="natOne" className="resouce-group" />
+          <h3>Fittness</h3>
         </div>
       </div>
     </div>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Gallery from "../components/homePageComponents/GallerySection";
 import About from "../components/homePageComponents/AboutSection";
+import ResourcesSection from "../components/homePageComponents/ResourcesSection";
 
 const HomePage = () => {
   return (
     <div>
       <Gallery />
       <About />
+      <ResourcesSection />
     </div>
   );
 };
