948472c4f63e8bc28a101e7f6bd56fcd7aa9728f
EChilin5 <edgar3ac@gmail.com>
2023-08-07 20:11:14 -0700

    meal progress section

--- frontend/src/components/ProgreesPageComponents/MealPlans.js
+++ frontend/src/components/ProgreesPageComponents/MealPlans.js
@@ -6,15 +6,21 @@ const MealPlans = () => {
     <div>
       <div className="meal-plan">
         <div className="meal-plan-header">
-          <h4>Day</h4>
-          <h4>Weight</h4>
-          <h4>Calories</h4>
-          <h4>Meals</h4>
+          <h4>8/7/23</h4>
+          <h4>150 lbs</h4>
+          <h4>8000 calories</h4>
+          <h4>All Meals</h4>
+          {temp.map((meals) => {
+            return (
+              <div className="meal-plan-individual" key={meals}>
+                <h5>{meals}.</h5>
+                <h5>Pizza</h5>
+                <h5>500 calories</h5>
+              </div>
+            );
+          })}
         </div>
-        <div className="meal-plan-rows">
-          <h5>Pizza</h5>
-          <h5>500 calories</h5>
-        </div>
+        <div className="meal-plan-rows"></div>
       </div>
     </div>
   );
