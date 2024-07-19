2400a08af394c880a92ba97714ca94f15f9651c4
Edgar Chilin <edgar3ac@gmail.com>
2024-07-18 21:28:48 -0700

    new comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import HomePage from "./pages/HomePage";
 
 function App() {
-  return (// test 
+  return (// test 56
     <div className="App">
       <NavigationBar />
     </div>
